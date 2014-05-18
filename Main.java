ba9e0a943709a06fc663c4f171367e3637ef32ca
Brindesable <ollivier.kilian@gmail.com>
2014-05-18 17:55:10 +0200

    Correction Money

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 
 public class Main {
 
+	private static Money money1, money2;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		money1 = new Money();
+		money2 = new Money(2, "KIL");
+		
+		System.out.println(money1);
+		System.out.println(money2);
+		money1.add(money1);
+		System.out.println(money1);
+		money1.add(new Money(3, "EUR"));
+		System.out.println(money1);
+		money1.sub(new Money(1, "EUR"));
+		System.out.println(money1);
 	}
 
 }
--- Money.java
+++ Money.java
@@ -1,4 +1,60 @@
 
 public class Money {
-
+	
+	private float _amount;
+	private String _currency;
+	
+	public Money() {
+		this._amount = 0;
+		this._currency = "EUR";
+	}
+	
+	public Money(float amount, String currency) {
+		this._amount = amount;
+		this._currency = currency;
+	}
+	
+	public float getAmount() {
+		return this._amount;
+	}
+	
+	public String getCurrency() {
+		return this._currency;
+	}
+	
+	public boolean checkCurrency(Money m) {
+		if(m._currency == this._currency)
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean checkCurrency(String s) {
+		if(s == this._currency)
+			return true;
+		else
+			return false;
+	}
+	
+	public void add(float amount) {
+		this._amount += amount;
+	}
+	
+	public void sub(float amount) {
+		this._amount -= amount;
+	}
+	
+	public void add(Money m) {
+		if(this.checkCurrency(m))
+			this.add(m._amount);
+	}
+	
+	public void sub(Money m) {
+		if(this.checkCurrency(m))
+			this.sub(m._amount);
+	}
+	
+	public String toString() {
+		return this._amount + " " + this._currency;
+	}
 }
