b1f937db8d27d5de82858dcfb9b696057f3035c5
Brindesable <ollivier.kilian@gmail.com>
2014-05-22 14:31:50 +0200

    .gitignore added

--- MoneyTest.java
+++ MoneyTest.java
@@ -30,21 +30,33 @@ public class MoneyTest {
 	}
 	
 	@Test
-	public void test3() {		
-		money1.add(money1);
-		assertEquals("0.0 EUR", money1.toString());
-		assertEquals("2.0 EUR", money1.toString());
+	public void test3() {	
+		try{
+			money1.add(money1);
+			assertEquals("Error potato1 in system", "0.0 EUR", money1.toString());
+			assertEquals("Error potato2 in system", "2.0 EUR", money1.toString());
+			System.out.println("3passed");
+		}catch(AssertionError e){
+			System.out.println("3error");
+			throw e;
+		}
 	}
 	
 	@Test
-	public void test4() {		
-		money1.add(money1);
-		money1.add(new Money(3, "EUR"));
-		assertEquals("3.0 EUR", money1.toString());
+	public void test4() {
+		try {
+			money1.add(money1);
+			money1.add(new Money(3, "EUR"));
+			assertEquals("3.0 EUR", money1.toString());
+			System.out.println("4passed");
+		}catch(AssertionError e){
+			System.out.println("4error");
+			throw e;
+		}
 	}
 	
 	@Test
-	public void test5() {		
+	public void test5() {
 		money1.add(money1);
 		money1.add(new Money(3, "EUR"));
 		money1.sub(new Money(1, "EUR"));
